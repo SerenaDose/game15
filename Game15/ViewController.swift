2d1143c51391fba62a5f765cf4ad8052b9c4280b
Serena Dose <serena.dose@gmail.com>
2017-08-25 15:41:58 +0200

    Initial Commit

---
+++ Game15/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game15
+//
+//  Created by Serena Dose on 25/08/2017.
+//  Copyright © 2017 Serena Dsoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
